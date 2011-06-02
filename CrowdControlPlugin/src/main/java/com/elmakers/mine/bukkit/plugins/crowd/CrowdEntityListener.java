ae1e916eb36783946641fe1f08ff37b1fe512149
Andrew Querol <andrewquerol@gmail.com>
2011-06-01 19:09:53 -0500

    Start of major overhaul.
    Change-Id: Iaea2cfdd7f9e2278099958996e0a01af5b204456

--- CrowdControlPlugin/src/main/java/com/elmakers/mine/bukkit/plugins/crowd/CrowdEntityListener.java
+++ CrowdControlPlugin/src/main/java/com/elmakers/mine/bukkit/plugins/crowd/CrowdEntityListener.java
@@ -1,31 +1,57 @@
 package com.elmakers.mine.bukkit.plugins.crowd;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import org.bukkit.entity.CreatureType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityListener;
 
-import com.elmakers.mine.bukkit.plugins.crowd.dao.ControlledWorld;
-import com.elmakers.mine.bukkit.plugins.crowd.dao.WorldData;
+/*
+ * Entity listener, overrides the default entity spawning.
+ * 
+ * @author Andrew Querol(WinSock)
+ * 
+ */
 
-public class CrowdEntityListener extends EntityListener
-{
-    protected Controller  controller;
-    private CrowdControlPlugin plugin;
+public class CrowdEntityListener extends EntityListener {
+	private CrowdControlPlugin plugin;
+	private Random generator = new Random();
+	private Set<SpawnInfo> spawnEntities = new HashSet<SpawnInfo>();
 
-    public void initialize(final CrowdControlPlugin plugin, Controller controller)
-    {
-    	this.plugin = plugin;
-        this.controller = controller;
-    }
+	public CrowdEntityListener(CrowdControlPlugin plugin) {
+		this.plugin = plugin;
+	}
 
-    @Override
-    public void onCreatureSpawn(CreatureSpawnEvent event)
-    {
-        ControlledWorld worldData = plugin.getDatabase().find(ControlledWorld.class).where().idEq(plugin.getDatabase().find(WorldData.class).where().idEq(event.getLocation().getWorld().getName()).findUnique()).findUnique();
-        if (worldData == null)
-        {
-            return;
-        }
+	@Override
+	public void onCreatureSpawn(CreatureSpawnEvent event) {
+		if (event.getEntity() instanceof LivingEntity) {
+			for (SpawnInfo i : spawnEntities) {
+				if (i.getEntity() == (LivingEntity)event.getEntity()) {
+					spawnEntities.remove(i);
+					return; // Entity spawned by us, do not re-randomize the creature
+				}
+			}
 
-        controller.controlSpawnEvent(worldData, event);
-    }
+			SpawnInfo info = new SpawnInfo();
+			info.setEnv(event.getLocation().getWorld().getEnvironment());
+			info.setLocation(event.getLocation());
+
+			// Pick a random creature overriding default spawn
+			int random = generator.nextInt(CreatureType.values().length);
+			info.setType(CreatureType.values()[random]);
+
+			if (plugin.ruleHandler.passesRules(info)) {
+				info.spawn();
+				if (info.getEntity() != null)
+				{
+					spawnEntities.add(info);
+				}
+			}
+
+			event.setCancelled(true);
+		}
+	}
 }
--- CrowdControlPlugin/src/main/java/com/elmakers/mine/bukkit/plugins/crowd/dao/MessageLevel.java
+++
@@ -1,6 +0,0 @@
-package com.elmakers.mine.bukkit.plugins.crowd.dao;
-
-public enum MessageLevel
-{
-    DEBUG, ERROR, INFO, WARNING
-}
