5500cf54cf663c8e048915ed271802e784c2144f
Andrew Querol <andrewquerol@gmail.com>
2011-06-03 13:18:09 -0500

    Add spawn Light/Material rules.
    Change-Id: I6b72a9ace86766f12c3d52c2833cf79d4497e248

---
+++ CrowdControlPlugin/src/main/java/com/elBukkit/mine/bukkit/plugins/crowd/rules/SpawnLightRule.java
@@ -0,0 +1,56 @@
+package com.elBukkit.mine.bukkit.plugins.crowd.rules;
+
+import java.util.Set;
+
+import org.bukkit.World;
+import org.bukkit.entity.CreatureType;
+
+import com.elBukkit.mine.bukkit.plugins.crowd.SpawnInfo;
+
+/*
+ * A rule that controls spawning based on light levels.
+ * 
+ * @author Andrew Querol(WinSock)
+ */
+
+public class SpawnLightRule implements SpawnRule {
+
+	private Set<World> worlds;
+	private CreatureType type;
+	
+	private int min,max;
+
+	public SpawnLightRule(int min, int max, Set<World> worlds, CreatureType type) {
+		this.worlds = worlds;
+		this.type = type;
+		
+		this.min = min;
+		this.max = max;
+	}
+
+	public boolean spawn(SpawnInfo info) {
+		if (info.getLocation().getBlock().getLightLevel() > min)
+		{
+			if (info.getLocation().getBlock().getLightLevel() < max)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean checkWorld(World world) {
+		if (worlds.contains(world)) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean checkCreatureType(CreatureType type) {
+		if (this.type == type) {
+			return true;
+		}
+		return false;
+	}
+
+}
---
+++ CrowdControlPlugin/src/main/java/com/elBukkit/mine/bukkit/plugins/crowd/rules/SpawnMaterialRule.java
@@ -0,0 +1,47 @@
+package com.elBukkit.mine.bukkit.plugins.crowd.rules;
+
+import java.util.Set;
+
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.CreatureType;
+
+import com.elBukkit.mine.bukkit.plugins.crowd.SpawnInfo;
+
+public class SpawnMaterialRule implements SpawnRule {
+
+	private Set<World> worlds;
+	private CreatureType type;
+	
+	private Set<Material> materials;
+
+	public SpawnMaterialRule(Set<Material> materials, Set<World> worlds, CreatureType type) {
+		this.worlds = worlds;
+		this.type = type;
+		
+		this.materials = materials;
+	}
+
+	public boolean spawn(SpawnInfo info) {
+		if (materials.contains(info.getLocation().getBlock().getType()))
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public boolean checkWorld(World world) {
+		if (worlds.contains(world)) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean checkCreatureType(CreatureType type) {
+		if (this.type == type) {
+			return true;
+		}
+		return false;
+	}
+
+}
