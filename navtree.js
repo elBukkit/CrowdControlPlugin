04f9ddb6ac392abb17ba6d30af697e6cd9d38d13
Andrew Querol <andrewquerol@gmail.com>
2011-06-30 12:27:43 -0500

    doxygen

---
+++ navtree.js
@@ -0,0 +1,366 @@
+var NAVTREE =
+[
+  [ "Crowd Control Plugin", "index.html", [
+    [ "Class List", "annotated.html", [
+      [ "com.elbukkit.plugins.crowd.creature.BaseInfo", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1creature_1_1_base_info.html", null ],
+      [ "com.elbukkit.plugins.crowd.events.CreatureAttackEvent", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1events_1_1_creature_attack_event.html", null ],
+      [ "com.elbukkit.plugins.crowd.creature.CreatureHandler", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1creature_1_1_creature_handler.html", null ],
+      [ "com.elbukkit.plugins.crowd.events.CreatureMoveEvent", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1events_1_1_creature_move_event.html", null ],
+      [ "com.elbukkit.plugins.crowd.events.CreatureSpawnEvent", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1events_1_1_creature_spawn_event.html", null ],
+      [ "com.elbukkit.plugins.crowd.CrowdCommand", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1_crowd_command.html", null ],
+      [ "com.elbukkit.plugins.crowd.CrowdControlPlugin", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1_crowd_control_plugin.html", null ],
+      [ "com.elbukkit.plugins.crowd.creature.CrowdCreature", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1creature_1_1_crowd_creature.html", null ],
+      [ "com.elbukkit.plugins.crowd.CrowdEntityListener", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1_crowd_entity_listener.html", null ],
+      [ "com.elbukkit.plugins.crowd.events.CrowdListener", "interfacecom_1_1elbukkit_1_1plugins_1_1crowd_1_1events_1_1_crowd_listener.html", null ],
+      [ "com.elbukkit.plugins.crowd.CrowdWorldListener", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1_crowd_world_listener.html", null ],
+      [ "com.elbukkit.plugins.crowd.creature.DamageHandler", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1creature_1_1_damage_handler.html", null ],
+      [ "com.alta189.sqlLibraryV2.SQLite.DatabaseHandler", "classcom_1_1alta189_1_1sql_library_v2_1_1_s_q_lite_1_1_database_handler.html", null ],
+      [ "com.elbukkit.plugins.crowd.Info", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1_info.html", null ],
+      [ "com.elbukkit.plugins.crowd.rules.MaxRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_max_rule.html", null ],
+      [ "com.elbukkit.plugins.crowd.creature.MovementHandler", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1creature_1_1_movement_handler.html", null ],
+      [ "com.elbukkit.plugins.crowd.rules.MovementLocationRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_movement_location_rule.html", null ],
+      [ "com.elbukkit.plugins.crowd.rules.Rule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_rule.html", null ],
+      [ "com.elbukkit.plugins.crowd.RuleHandler", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1_rule_handler.html", null ],
+      [ "com.elbukkit.plugins.crowd.rules.SpawnEnvironmentRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_spawn_environment_rule.html", null ],
+      [ "com.elbukkit.plugins.crowd.creature.SpawnHandler", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1creature_1_1_spawn_handler.html", null ],
+      [ "com.elbukkit.plugins.crowd.rules.SpawnHeightRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_spawn_height_rule.html", null ],
+      [ "com.elbukkit.plugins.crowd.rules.SpawnLightRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_spawn_light_rule.html", null ],
+      [ "com.elbukkit.plugins.crowd.rules.SpawnLocationRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_spawn_location_rule.html", null ],
+      [ "com.elbukkit.plugins.crowd.rules.SpawnMaterialRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_spawn_material_rule.html", null ],
+      [ "com.elbukkit.plugins.crowd.rules.SpawnReplaceRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_spawn_replace_rule.html", null ],
+      [ "com.elbukkit.plugins.crowd.rules.SpawnTimeRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_spawn_time_rule.html", null ],
+      [ "com.alta189.sqlLibraryV2.SQLite.sqlCore", "classcom_1_1alta189_1_1sql_library_v2_1_1_s_q_lite_1_1sql_core.html", null ],
+      [ "com.elbukkit.plugins.crowd.rules.TargetPlayerRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_target_player_rule.html", null ],
+      [ "com.elbukkit.plugins.crowd.ThreadSafe", "interfacecom_1_1elbukkit_1_1plugins_1_1crowd_1_1_thread_safe.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "com.elbukkit.plugins.crowd.creature.BaseInfo", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1creature_1_1_base_info.html", null ],
+      [ "com.elbukkit.plugins.crowd.events.CreatureAttackEvent", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1events_1_1_creature_attack_event.html", null ],
+      [ "com.elbukkit.plugins.crowd.creature.CreatureHandler", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1creature_1_1_creature_handler.html", null ],
+      [ "com.elbukkit.plugins.crowd.events.CreatureMoveEvent", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1events_1_1_creature_move_event.html", null ],
+      [ "com.elbukkit.plugins.crowd.events.CreatureSpawnEvent", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1events_1_1_creature_spawn_event.html", null ],
+      [ "com.elbukkit.plugins.crowd.CrowdCommand", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1_crowd_command.html", null ],
+      [ "com.elbukkit.plugins.crowd.CrowdControlPlugin", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1_crowd_control_plugin.html", null ],
+      [ "com.elbukkit.plugins.crowd.creature.CrowdCreature", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1creature_1_1_crowd_creature.html", null ],
+      [ "com.elbukkit.plugins.crowd.CrowdEntityListener", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1_crowd_entity_listener.html", null ],
+      [ "com.elbukkit.plugins.crowd.events.CrowdListener", "interfacecom_1_1elbukkit_1_1plugins_1_1crowd_1_1events_1_1_crowd_listener.html", null ],
+      [ "com.elbukkit.plugins.crowd.CrowdWorldListener", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1_crowd_world_listener.html", null ],
+      [ "com.elbukkit.plugins.crowd.creature.DamageHandler", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1creature_1_1_damage_handler.html", null ],
+      [ "com.alta189.sqlLibraryV2.SQLite.DatabaseHandler", "classcom_1_1alta189_1_1sql_library_v2_1_1_s_q_lite_1_1_database_handler.html", null ],
+      [ "com.elbukkit.plugins.crowd.Info", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1_info.html", null ],
+      [ "com.elbukkit.plugins.crowd.creature.MovementHandler", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1creature_1_1_movement_handler.html", null ],
+      [ "com.elbukkit.plugins.crowd.rules.Rule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_rule.html", [
+        [ "com.elbukkit.plugins.crowd.rules.MaxRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_max_rule.html", null ],
+        [ "com.elbukkit.plugins.crowd.rules.MovementLocationRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_movement_location_rule.html", null ],
+        [ "com.elbukkit.plugins.crowd.rules.SpawnEnvironmentRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_spawn_environment_rule.html", null ],
+        [ "com.elbukkit.plugins.crowd.rules.SpawnHeightRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_spawn_height_rule.html", null ],
+        [ "com.elbukkit.plugins.crowd.rules.SpawnLightRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_spawn_light_rule.html", null ],
+        [ "com.elbukkit.plugins.crowd.rules.SpawnLocationRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_spawn_location_rule.html", null ],
+        [ "com.elbukkit.plugins.crowd.rules.SpawnMaterialRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_spawn_material_rule.html", null ],
+        [ "com.elbukkit.plugins.crowd.rules.SpawnReplaceRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_spawn_replace_rule.html", null ],
+        [ "com.elbukkit.plugins.crowd.rules.SpawnTimeRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_spawn_time_rule.html", null ],
+        [ "com.elbukkit.plugins.crowd.rules.TargetPlayerRule", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules_1_1_target_player_rule.html", null ]
+      ] ],
+      [ "com.elbukkit.plugins.crowd.RuleHandler", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1_rule_handler.html", null ],
+      [ "com.elbukkit.plugins.crowd.creature.SpawnHandler", "classcom_1_1elbukkit_1_1plugins_1_1crowd_1_1creature_1_1_spawn_handler.html", null ],
+      [ "com.alta189.sqlLibraryV2.SQLite.sqlCore", "classcom_1_1alta189_1_1sql_library_v2_1_1_s_q_lite_1_1sql_core.html", null ],
+      [ "com.elbukkit.plugins.crowd.ThreadSafe", "interfacecom_1_1elbukkit_1_1plugins_1_1crowd_1_1_thread_safe.html", null ]
+    ] ],
+    [ "Class Members", "functions.html", null ],
+    [ "Packages", "namespaces.html", [
+      [ "com", "namespacecom.html", null ],
+      [ "com.alta189", "namespacecom_1_1alta189.html", null ],
+      [ "com.alta189.sqlLibraryV2", "namespacecom_1_1alta189_1_1sql_library_v2.html", null ],
+      [ "com.alta189.sqlLibraryV2.SQLite", "namespacecom_1_1alta189_1_1sql_library_v2_1_1_s_q_lite.html", null ],
+      [ "com.elbukkit", "namespacecom_1_1elbukkit.html", null ],
+      [ "com.elbukkit.plugins", "namespacecom_1_1elbukkit_1_1plugins.html", null ],
+      [ "com.elbukkit.plugins.crowd", "namespacecom_1_1elbukkit_1_1plugins_1_1crowd.html", null ],
+      [ "com.elbukkit.plugins.crowd.creature", "namespacecom_1_1elbukkit_1_1plugins_1_1crowd_1_1creature.html", null ],
+      [ "com.elbukkit.plugins.crowd.events", "namespacecom_1_1elbukkit_1_1plugins_1_1crowd_1_1events.html", null ],
+      [ "com.elbukkit.plugins.crowd.rules", "namespacecom_1_1elbukkit_1_1plugins_1_1crowd_1_1rules.html", null ]
+    ] ],
+    [ "Package Functions", "namespacemembers.html", null ],
+    [ "File List", "files.html", [
+      [ "src/main/java/com/alta189/sqlLibraryV2/SQLite/DatabaseHandler.java", "_database_handler_8java.html", null ],
+      [ "src/main/java/com/alta189/sqlLibraryV2/SQLite/sqlCore.java", "sql_core_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/CrowdCommand.java", "_crowd_command_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/CrowdControlPlugin.java", "_crowd_control_plugin_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/CrowdEntityListener.java", "_crowd_entity_listener_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/CrowdWorldListener.java", "_crowd_world_listener_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/Info.java", "_info_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/RuleHandler.java", "_rule_handler_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/ThreadSafe.java", "_thread_safe_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/creature/BaseInfo.java", "_base_info_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/creature/CreatureHandler.java", "_creature_handler_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/creature/CrowdCreature.java", "_crowd_creature_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/creature/DamageHandler.java", "_damage_handler_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/creature/MovementHandler.java", "_movement_handler_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/creature/Nature.java", "_nature_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/creature/SpawnHandler.java", "_spawn_handler_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/events/CreatureAttackEvent.java", "_creature_attack_event_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/events/CreatureMoveEvent.java", "_creature_move_event_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/events/CreatureSpawnEvent.java", "_creature_spawn_event_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/events/CrowdListener.java", "_crowd_listener_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/rules/MaxRule.java", "_max_rule_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/rules/MovementLocationRule.java", "_movement_location_rule_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/rules/Rule.java", "_rule_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/rules/SpawnEnvironmentRule.java", "_spawn_environment_rule_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/rules/SpawnHeightRule.java", "_spawn_height_rule_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/rules/SpawnLightRule.java", "_spawn_light_rule_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/rules/SpawnLocationRule.java", "_spawn_location_rule_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/rules/SpawnMaterialRule.java", "_spawn_material_rule_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/rules/SpawnReplaceRule.java", "_spawn_replace_rule_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/rules/SpawnTimeRule.java", "_spawn_time_rule_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/rules/TargetPlayerRule.java", "_target_player_rule_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/rules/Time.java", "_time_8java.html", null ],
+      [ "src/main/java/com/elbukkit/plugins/crowd/rules/Type.java", "_type_8java.html", null ]
+    ] ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
